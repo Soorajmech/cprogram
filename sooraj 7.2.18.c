080884caf4cb21630ca11e94229293a77a9e65ff
Soorajmech <35765128+Soorajmech@users.noreply.github.com>
2018-02-07 16:28:41 +0530

    Create sooraj 7.2.18.c

---
+++ sooraj 7.2.18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char s;
+    g=getchar();
+    
+    if(s=='a'|s=='e'|s=='i'|s=='o'|s=='u'){
+        printf("Vowel");
+    }else {
+        printf("Consonent");
+    }
+}
