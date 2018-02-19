8082ecffab93f351a5afd7c79f83894920553c8d
Soorajmech <35765128+Soorajmech@users.noreply.github.com>
2018-02-19 15:43:06 +0530

    Create sooraj21.c

---
+++ sooraj21.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,a ,d,sum;
+sum=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=n;i>1;i++);
+{
+sum=sum+(a+(n-1)*d);
+}
+sum=sum+a;
+printf("%d",sum);
+return 0;
+}
