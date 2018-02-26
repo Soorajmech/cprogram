b59eb76ab0edbfb29698990b22b388333b1d99b8
Soorajmech <35765128+Soorajmech@users.noreply.github.com>
2018-02-26 15:48:12 +0530

    Create sooraj38.c

---
+++ sooraj38.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int v=0,j=1,n,limit,i;
+    printf("enter the limit");
+    scanf("%d",&limit);
+    printf("%d%d",v,j);
+    for(i=2;i<=limit;i++)
+    {
+        n=v+j;
+        f=j;
+        j=n;
+        printf("%d",n);
+    }
+}
