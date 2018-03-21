ea1e24ff4aa8109471f10ee0daee14f72b23dedf
Soorajmech <35765128+Soorajmech@users.noreply.github.com>
2018-03-21 16:14:20 +0530

    Create sooraj74.c

---
+++ sooraj74.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	float num;
+	int j;
+  
+  printf("\n enter the num:");
+	scanf("%f",&num);
+  
+	j=(int)(num+0.5);
+  
+	printf("\n%d",j);
+  getch();
+	return 0;
+}
